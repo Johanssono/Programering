59a0f445f0a7c64cccc07f086b50950e20087ed4
Johanssono <ottojohanssonjobb@gmail.com>
2023-09-11 13:47:56 +0200

    klar

--- While slingor/7060.py
+++ While slingor/7060.py
@@ -1,4 +1,3 @@
 i = 6
 a=input("Hur många heltal vill du ska skrivas ut?")
 b=input("Vilket ska vadra det minsta talet")
-
---
+++ While slingor/7061.py
@@ -0,0 +1,16 @@
+svar = input("Gisssa ett nummer! ")
+
+while svar != 7:
+    svar= input("Försök igen!")
+    svar = int(svar)
+    if svar < 10:
+        svar = input ("Kankse lite lågt, gissa igen! ")
+    elif svar > 83:
+        svar = input ("Något för högt kanske, försök en igen! ")
+    elif svar == 68:
+        svar = input("Nära, men inte rätt, en gång till! ")
+    elif svar == 69:
+        svar = input("Aahhhh, du trodde! ")
+    elif svar == 70:
+        svar = input("Nära, men inte rätt, igen! ")
+print("grattis, du lyckades slösa bort ditt liv med en värdelös lek")
