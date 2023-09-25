d616edddec9bf2e68ea97825c553e79fbfbb52f0
Johanssono <ottojohanssonjobb@gmail.com>
2023-09-25 13:41:32 +0200

    fixade klart spelet jag jobbat med

---
+++ 7070 slumptal/7070 sten,sax,påse v2.py
@@ -0,0 +1,53 @@
+svar = input("Vill du spela ett spel? j/n ")
+
+import random
+
+mylist = ["sten", "sax", "påse"]
+
+
+
+
+
+while svar =="j" or svar == "ja":
+    if svar == "j" or svar == "ja":
+        svar = input("sten! sax! påse!... ")
+        
+        a = random.randrange(1,4)
+        if a == 1 and svar == "sten":
+            print ("sten")
+            svar =input("Det blev lika, vill du spela igen?  j/n ")
+            
+        elif a == 1 and svar == "sax":
+            print ("sten")
+            svar = input("Jag vann! Vill du spela igen?  j/n ")
+            
+        elif a == 1 and svar == "påse":
+            print ("sten")
+            svar = input("Du vann! grattis! Vill du spela igen?  j/n ")
+            
+        
+        elif a == 2 and svar == "sax":
+            print ("sax")
+            svar = input("Det blev lika, vill du spela igen?  j/n ")
+            
+        elif a == 2 and svar == "påse":
+            print ("sax")
+            svar = input("Jag vann! Vill du spela igen?  j/n ")
+            
+        elif a == 2 and svar == "sten":
+            print ("sax")
+            svar = input("Du vann! grattis! Vill du spela igen?  j/n ")
+            
+        elif a == 3 and svar == "påse":
+            print ("påse")
+            svar = input("Det blev lika, vill du spela igen?  j/n ")
+            
+        elif a == 3 and svar == "sten":
+            print ("påse")
+            svar= input("Jag vann! Vill du spela igen?  j/n ")
+            
+        else:
+            print ("påse")
+            svar =input("Du vann! grattis! Vill du spela igen?  j/n ")
+            
+print ("vad tråkigt, hej då")
--- 7070 slumptal/7070 sten,sax,påse.py
+++ 7070 slumptal/7070 sten,sax,påse.py
@@ -7,49 +7,44 @@ mylist = ["sten", "sax", "påse"]
 
 
 
+while svar == "j" or svar == "ja":
+    if svar == "j" or svar == "ja":
+        svar = input("sten! sax! påse!... ")
+        game = random.choice(mylist)
+        if game == "sten" and svar == "sten":
+            print (game)
+            svar = input("Det blev lika, vill du spela igen?  j/n ")
+            
+        elif game == "sten" and svar == "sax":
+            print (game)
+            svar = input("Det blev lika, vill du spela igen?  j/n ")
+            
+        elif game == "sten" and svar == "påse":
+            print (game)
+            svar = input("Du vann! grattis! Vill du spela igen?  j/n ")
 
-print("VÄLKOMMEN")
-if svar == "j":
-    svar = input("sten! sax! påse!... ")
-    game = random.choice(mylist)
-    if game == "sten" and svar == "sten":
-        print = game
-        input("Det blev lika, vill du spela igen?  j/n ")
-        
-    elif game == "sten" and svar == "sax":
-        print = game
-        input("Jag van! Vill du spela igen?  j/n ")
-        
-    elif game == "sten" and svar == "påse":
-        print = game
-        input("Du van, grattis! Vill du spela igen?  j/n ")
+        elif game == "sax" and svar == "sax":
+            print (game)
+            svar = input("Det blev lika, vill du spela igen?  j/n ")
+            
+        elif game == "sax" and svar == "påse":
+            print (game)
+            svar = input("Jag vann! vill du spela igen?  j/n ")
+            
+        elif game == "sax" and svar == "sten":
+            print (game)
+            svar = input("Du vann! grattis! Vill du spela igen?  j/n ")
+            
+        elif game == "påse" and svar == "påse":
+            print (game)
+            svar = input("Det blev lika, vill du spela igen?  j/n ")
+            
+        elif game == "påse" and svar == "sten":
+            print (game)
+            svar = input("Jag vann! vill du spela igen?  j/n ")
+            
+        else:
+            print (game)
+            svar = input("Du vann! grattis! Vill du spela igen?  j/n ")
 
-        
-    elif game == "sax" and svar == "sax":
-        print = game
-        input("Det blev lika, vill du spela igen?  j/n ")
-        
-    elif game == "sax" and svar == "påse":
-        print = game
-        input("Jag van! Vill du spela igen?  j/n ")
-        
-    elif game == "sax" and svar == "sten":
-        print = game
-        input("Du van, grattis! Vill du spela igen?  j/n ")
-        
-
-    elif game == "påse" and svar == "påse":
-        print = game
-        input("Det blev lika, vill du spela igen?  j/n ")
-        
-    elif game == "påse" and svar == "sten":
-        print = game
-        input("Jag van! Vill du spela igen?  j/n ")
-        
-    elif game == "påse" and svar == "sax":
-        print = game
-        input("Du van, grattis! Vill du spela igen?  j/n ")
-        
-    else:
-        print ("DU vann, grattis!!")
-    
+print ("vad tråkigt, hej då")
