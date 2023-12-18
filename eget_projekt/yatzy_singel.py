730fa099d473e03cb6491b199e0c3321c8370f9f
Johanssono <ottojohanssonjobb@gmail.com>
2023-12-18 12:16:58 +0100

    ändringar

--- eget_projekt/yatzy_singel.py
+++ eget_projekt/yatzy_singel.py
@@ -1,52 +1,49 @@
 import random
 
-spelare = []
-tärningar = []
-sparade = []
+player = []
+dice = []
+saved = []
+antal = 5
 
 
-svar = input("Vill du spela ett spel? j/n: ")
+namn = input("Skriv namn på spelare: ")
+player.append(namn)
+
+
+svar = input("Hej", namn, "Vill du spela ett spel? j/n: ")
 
 if svar == "j":
-    namn = input("Skriv namn på spelare: ")
-    spelare.append(namn)
+    kast = input("Vill du kasta tärningarna? j/n: ")
 
-else:
-    print("ok, ha en bra dag")
-
-kast = input("Vill du kasta tärningarna? j/n: ")
-
-if kast == "j":
-    tärning_1 = random.randrange(1, 7)
-    tärningar.append(tärning_1)
-
-    tärning_2 = random.randrange(1, 7)
-    tärningar.append(tärning_2)
     
-    tärning_3 = random.randrange(1, 7)
-    tärningar.append(tärning_3)
-    
-    tärning_4 = random.randrange(1, 7)
-    tärningar.append(tärning_4)
+    while kast == "j":
+        while antal > 0:
+            tärning_1 = random.randrange(1, 7)
+            dice.append(tärning)
+            antal = antal - 1
+            print(dice)
 
-    tärning_5 = random.randrange(1, 7)
-    tärningar.append(tärning_5)
+            spara = int(input("vilka vill du spara? 1, 2, 3, 4, 5 eller 6: "))
 
-else:
-    print("ok, ha en bra dag")
+            for tal in dice:
+                if (tal == spara):
+                    saved.append(tal)
+                    dice.remove(tal)
 
-print(tärningar)
+            print(dice)
+            antal = 5 - len(dice)
+            kast = input("Vill du kasta tärningarna? j/n: ")
 
-spara = int(input("vilka vill du spara? 1, 2, 3, 4, 5 eller 6: "))
+else print("hej då")
 
-for tal in tärningar:
-    if (tal == spara):
-        sparade.append(tal)
-        tärningar.remove(tal)
+#tärning_2 = random.randrange(1, 7)
+#dice.append(tärning_2)
 
-for antal in tärningar:
-    tärning_1 = random.randrange(1, 7)
-    tärningar.append(tärning_1)
-    antal = antal - 1
+#tärning_3 = random.randrange(1, 7)
+#dice.append(tärning_3)
 
-print(tärningar)
+#tärning_4 = random.randrange(1, 7)
+#dice.append(tärning_4)
+
+#tärning_5 = random.randrange(1, 7)
+#dice.append(tärning_5)
