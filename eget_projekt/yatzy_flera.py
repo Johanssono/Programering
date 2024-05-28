13c19c567928388a0636ca756f179d51d3184868
Johanssono <ottojohanssonjobb@gmail.com>
2024-05-28 12:12:38 +0200

    slut fix

--- eget_projekt/yatzy_flera.py
+++ eget_projekt/yatzy_flera.py
@@ -9,31 +9,22 @@ class Player:
         """Totala mängden poäng som spelaren har"""
         self.points_total = 0
         """En lista med alla alternativ som spelaren kan välja ifrån, används för att räkna ut valmöjligheter för spelaren"""
-        self.table = {1: "Ettor", 2: "Tvåor", 3: "Treor", 4: "Fyror", 5: "Femmor", 6: "Sexor", 7: "Par", 8: "Tretal", 9: "Fyrtal", 10: "Två par", 11: "Kåk", 12:"Liten stege", 13:"Stor Stege", 14: "Yatzy", 15: "Chans"}
+        self.table = {1: "Ettor", 2: "Tvåor", 3: "Treor", 4: "Fyror", 5: "Femmor", 6: "Sexor", 7: "Par", 8: "Tretal", 9: "Fyrtal", 10: "Två par", 11: "Kåk", 12: "Liten stege", 13: "Stor Stege", 14: "Yatzy", 15: "Chans"}
+#Behöver göra så att man den frågar om frågan om man skriver en bokstav när den ber om en siffra
 
 
-#Behöver göra så att man inte kan välja mer än dem alternativ man har
-
-#behöver också jöra så det inte ser så hämskt ut när man printar vem som börjar spela och alla deras "stats"
-
-def appropriation_of_options(potential, testö):
-    """Lägger till alternativ i listan choices där spelaren sedan väljer vilkår att lägga poängen på."""
+def count_of_options(potential):
+    """Lägger till alt ernativ i listan choices där spelaren sedan väljer vilkår att lägga poängen på."""
     for option in participant.table:
         if option == potential:
-            if participant.table[potential] not in choices:
-                amount_adder(testö)
-                choices.append(participant.table[potential])
+            if participant.table[potential] not in parikng_lot:
+                parikng_lot.append(participant.table[potential])
     return potential
 
 
-
-
-
-def amount_adder(testö):
-    choices.append(testö)
-    testö = testö + 1
-    return testö
-
+def appropriation_of_options(parikng_lot):
+    for amount in range(len(parikng_lot)):
+        choices[amount + 1] = parikng_lot[amount]
 
 
 def dice_counter(die):
@@ -81,9 +72,11 @@ def pair_counter(investment, participant, second_choice):
         checker = True
         while checker == True:
             print(second_choice)
-            investment_2 = int(input("Skriv siffran som du vill ha par på: "))
-            if investment_2 in second_choice:
-                checker = False
+            investment_2 = input("Skriv siffran som du vill ha par på: ")
+            if investment_2.isnumeric():
+                investment_2 = int(investment_2)
+                if investment_2 in second_choice and isinstance(investment, int):
+                    checker = False
         participant.points_total = participant.points_total + investment_2 * 2
     else:
         participant.points_total = participant.points_total + second_choice[0] * 2            
@@ -114,7 +107,7 @@ def counter_chooser(investment):
         smal_point_counter(investment, participant)
     elif investment == 7:
         pair_counter(investment, participant, second_choice)
-    elif investment  == 8 or investment == 9:
+    elif investment == 8 or investment == 9:
         average_point_counter(investment, participant)
     elif investment > 9:
         big_point_counter(investment, participant)
@@ -122,9 +115,15 @@ def counter_chooser(investment):
 def punishment():
     """Funktionen straffar spelaren om spelaren inte har tärningar som passar med något kvarstående alternativ, 
     detta genom att tvinga spelaren stryka ett alternativ ur listan med kvarvarande alternativ"""
-    print(participant.table)
-    sacrifice = input("Skriv siffran på den du vill stryka")
-    participant.table.pop(sacrifice)
+    checker = True
+    while checker == True:
+        print(participant.table)
+        sacrifice = input("Skriv siffran på den du vill stryka: ")
+        if sacrifice.isnumeric():
+            sacrifice = int(sacrifice)
+            if isinstance(sacrifice, int) and sacrifice <= len(participant.table):
+                participant.table.pop(sacrifice)
+                checker = False
 
 
 ###skriv en deffinition som kollar om den valda sattnsningen fungerar, om det inte gör det be dem välja sattsning igen.
@@ -150,7 +149,8 @@ throws = 3
 """En lista på sparade tärningar mellan kast"""
 saved = []
 """EN lista med alternativen som spelaren har att välja mellan"""
-choices = []
+choices = {}
+parikng_lot = []
 second_choice = []
 pair = []
 
@@ -159,14 +159,14 @@ bonus_countdown = 6
 
 player = []
 
-testö = 1
-
 checker = True
 
 while checker == True:
-    antal_spelare = int(input("Skriv in antalet spelare: "))
-    if isinstance(antal_spelare, int):
-        checker = False
+    antal_spelare = input("Skriv in antalet spelare: ")
+    if antal_spelare.isnumeric():
+        antal_spelare = int(antal_spelare)
+        if isinstance(antal_spelare, int):
+            checker = False
 
 while antal_spelare > 0:
     player_name = input("skriv namn på alla spelare. En spelare i taget tack, spelar-1 kommer slumpmässigt väljas ut: ")
@@ -180,8 +180,11 @@ game_time = True
 
 while game_time == True:
     for participant in player:
-        print(participant.name, participant.points, participant.points_total, participant.table, sep='')
-
+        print("Spelare: ", participant.name)
+        print("Poäng: ", participant.points_total)
+        print("Tabell: ", participant.table)
+        throws = 3
+        Quantity = 0
 
     #skapa while loopar så man kan srkiva fel utan att spelet avbryts
 
@@ -221,11 +224,16 @@ while game_time == True:
                             checker = True
                             while checker == True:
                                 print(dice)
-                                save = int(input("vilka vill du spara? 1, 2, 3, 4, 5 eller 6: "))
-                                if isinstance(save, int) and save < 7 and save > 0:
-                                    amount = int(input("Hur många av den valda siffran vill du spara? 1, 2, 3, 4 eller 5: "))
-                                    if isinstance(amount, int) and amount < 6 and amount > 0:
-                                        checker = False
+                                save = input("vilka vill du spara? 1, 2, 3, 4, 5 eller 6: ")
+                                if save.isnumeric():
+                                    save = int(save)
+                                    if isinstance(save, int) and save < 7 and save > 0:
+                                        while checker == True:
+                                            amount = input("Hur många av den valda siffran vill du spara? 1, 2, 3, 4 eller 5: ")
+                                            if amount.isnumeric():
+                                                amount = int(amount)
+                                                if isinstance(amount, int) and amount < 6 and amount > 0:
+                                                    checker = False
 
                             for i in range(len(dice) -1, -1, -1):
                                 if dice[i] == save and amount > 0:
@@ -249,30 +257,35 @@ while game_time == True:
 
                         print("sparade tärningar: ", saved)
                         
-                        checker = True
-                        while checker == True:
-                            remove_dice = input("Vill du ta bort några tärningar? j/n: ")
-                            if isinstance(remove_dice, str) and remove_dice == "j" or remove_dice == "n":
-                                checker = False
-
-                        while remove_dice == "j":
-                            delete = input("1, 2, 3, 4, 5 eller 6? ")
-                            if isinstance(delete, int) and delete < 7 and delete > 0:                        
-                                amount = int(input("Hur många av den valda siffran vill du spara? 1, 2, 3, 4 eller 5: "))
-                                if isinstance(amount, int) and amount < 6 and amount > 0:
-                                    for i in range(len(dice) -1, -1, -1):
-                                        if dice[i] == delete and amount > 0:
-                                            saved.remove(save)
-                                            dice.append(save)
-                                            amount = amount - 1
-                                    
-                                print("sparade tärningar: ", saved)
-                        
+                        if len(saved) > 0:
                             checker = True
                             while checker == True:
-                                remove_dice = input("Vill du ta bort några fler tärningar? j/n: ")
+                                remove_dice = input("Vill du ta bort några tärningar? j/n: ")
                                 if isinstance(remove_dice, str) and remove_dice == "j" or remove_dice == "n":
                                     checker = False
+
+                            while remove_dice == "j":
+                                delete = input("1, 2, 3, 4, 5 eller 6? ")
+                                if delete.isnumeric():
+                                    delete = int(delete)
+                                    if isinstance(delete, int) and delete < 7 and delete > 0:                        
+                                        amount = input("Hur många av den valda siffran vill du spara? 1, 2, 3, 4 eller 5: ")
+                                        if amount.isnumeric():
+                                            amount = int(amount)
+                                            if isinstance(amount, int) and amount < 6 and amount > 0:
+                                                for i in range(len(dice) -1, -1, -1):
+                                                    if dice[i] == delete and amount > 0:
+                                                        saved.remove(save)
+                                                        dice.append(save)
+                                                        amount = amount - 1
+                                            
+                                        print("sparade tärningar: ", saved)
+                            
+                                checker = True
+                                while checker == True:
+                                    remove_dice = input("Vill du ta bort några fler tärningar? j/n: ")
+                                    if isinstance(remove_dice, str) and remove_dice == "j" or remove_dice == "n":
+                                        checker = False
                         dice = []
 
                         dice.extend(saved)
@@ -316,7 +329,7 @@ while game_time == True:
                 for die in dice:
                     if die == potential_choice:
                         #tillägning_av_alternativ Python PEP: https://peps.python.org/pep-0008/
-                        appropriation_of_options(potential_choice, testö)
+                        count_of_options(potential_choice)
                 potential_choice = potential_choice + 1
 
 
@@ -334,9 +347,9 @@ while game_time == True:
                 for die in dice:
                     if dice.count(die) >= count:
                         potential = 6 + timer
-                        appropriation_of_options(potential, testö)
+                        count_of_options(potential)
                     elif dice.count(die) == 5:
-                        appropriation_of_options(15, testö)
+                        count_of_options(15)
                 count = count + 1
                 timer = timer + 1
 
@@ -346,11 +359,11 @@ while game_time == True:
                 timer = timer + 1
 
             if number_of_dice[0] == 1 and number_of_dice[1] == 1 and number_of_dice[2] == 1 and number_of_dice[3] == 1 and number_of_dice[4] == 1:
-                appropriation_of_options(12, testö)
+                count_of_options(12)
 
 
             elif  number_of_dice[1] == 1 and number_of_dice[2] == 1 and number_of_dice[3] == 1 and number_of_dice[4] == 1 and number_of_dice[5] == 1:
-                appropriation_of_options(13, testö)
+                count_of_options(13)
 
 
             potential = 1
@@ -366,31 +379,32 @@ while game_time == True:
 
             if len(pair) >= 4:
                 if pair[0] != pair[0 - 1] and len(pair) == 4:
-                    appropriation_of_options(10, testö)
+                    count_of_options(10)
 
                 if pair[0] != pair[0 - 1] and len(pair) == 5:
-                    appropriation_of_options(10, testö)
-                    appropriation_of_options(11, testö)
+                    count_of_options(10)
+                    count_of_options(11)
 
 
 
             for option in participant.table:
                 if option == 15:
                     if participant.table[15] not in choices:
-                        choices.append(participant.table[15])            
+                        parikng_lot.append(participant.table[15])            
             
-
-            print(choices)
+            appropriation_of_options(parikng_lot)
 
             if len(choices) == 0:
                 punishment()
             else:
                 checker = True
                 while checker == True:
-                    investment = int(input("Skriv siffran på alternativet du önskar välja: "))
-        #måste komma på ett sätt som spelar om loopen om man inte valt en siffra på dem alternativ man har
-                    if isinstance(investment, int):
-                        checker = False
+                    print(choices)
+                    investment = input("Skriv siffran på alternativet du önskar välja: ")
+                    if investment.isnumeric():
+                        investment = int(investment)
+                        if isinstance(investment, int) and investment <= len(choices):
+                            checker = False
                 
                 counter_chooser(investment)
 
@@ -406,13 +420,13 @@ while game_time == True:
             print("Dina sammanlagda poäng i alla vilkår: ", participant.points_total)
 
             dice = []
-            choices = []
+            choices = {}
             pair = []
             if len(player[-1].table) == 0:
                 game_time = False   
 
 highest_points = player[0].points_total
-winner = ""
+winner = player[0].name
 
 for participant in player:
     if participant.points_total > highest_points:
