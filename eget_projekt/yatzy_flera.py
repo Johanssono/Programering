fdc251ccb0bf03f69712f5e3189d8085ac64f109
Johanssono <ottojohanssonjobb@gmail.com>
2024-05-28 17:33:07 +0200

    städat komentarer

--- eget_projekt/yatzy_flera.py
+++ eget_projekt/yatzy_flera.py
@@ -12,9 +12,6 @@ class Player:
         self.table = {1: "Ettor", 2: "Tvåor", 3: "Treor", 4: "Fyror", 5: "Femmor", 6: "Sexor", 7: "Par", 8: "Tretal", 9: "Fyrtal", 10: "Två par", 11: "Kåk", 12: "Liten stege", 13: "Stor Stege", 14: "Yatzy", 15: "Chans"}
         self.table_index_counter = ("Ettor", "Tvåor", "Treor", "Fyror", "Femmor", "Sexor", "Par", "Tretal", "Fyrtal", "Två par", "Kåk", "Liten stege", "Stor Stege", "Yatzy", "Chans")
 
-#Behöver göra så att man den frågar om frågan om man skriver en bokstav när den ber om en siffra
-
-
 def count_of_options(potential):
     """Lägger till alt ernativ i listan choices där spelaren sedan väljer vilkår att lägga poängen på."""
     for option in participant.table:
@@ -34,7 +31,6 @@ def dice_counter(die):
     Quantity = dice.count(die)
     return Quantity
 
-    #Måste ändra om och returna ett svar och spara det svaret i en lista.
 
 def bonus_counter():
     """Räknar ut om spelaren har mött kriterierna för att motag bonusen"""
@@ -46,13 +42,11 @@ def yatzy_counter():
     participant.points_total = participant.points_total + 50
 
 def smal_point_counter(investment, participant):
-    #borde ientiligen inte använda "global", måste komma på ett annat sätt att ge funktionerna tillgång till listorna som dem arbetar med.
     """Funktionen räknar ut poängen om spelaren har valt att lägga poängen på vilkår 1-6 och lägger till dem i listan med poäng"""
     for die in dice:
         if die == investment:
             participant.points = participant.points + die
             participant.points_total = participant.points_total + die
-            #points_total räknas som en integer och kan inte använda sig av kommandot append.
     if investment == 14:
         yatzy_counter()
 
@@ -124,31 +118,13 @@ def punishment():
                 participant.table.pop(sacrifice)
                 checker = False
 
-
-###skriv en deffinition som kollar om den valda sattnsningen fungerar, om det inte gör det be dem välja sattsning igen.
-
-
-
-"""
-
-def type_checker_srt(variabel_tempo_str):
-if isinstance(variabel_tempo_str, str) and variabel_tempo_str == "j" or variabel_tempo_str == "n":
-
-def type_checker_int(variabel_tempo_int, variabel_amount_tempo):
-if isinstance(variabel_tempo_int, int) and variabel_tempo_int < variabel_amount_tempo 
-
-"""
-
-"En lista på spelare"
 dice = []
-"""En lista på antalet tärningar spelaren kan kasta efter att ha sparat tärningar"""
 number_of_dice = []
 Quantity = 0
 throws = 3
-"""En lista på sparade tärningar mellan kast"""
 saved = []
-"""EN lista med alternativen som spelaren har att välja mellan"""
 choices = {}
+"""EN lista som används för omvandla input till index för val av alteernativ"""
 parikng_lot = []
 second_choice = []
 pair = []
@@ -185,18 +161,9 @@ while game_time == True:
         throws = 3
         Quantity = 0
 
-    #skapa while loopar så man kan srkiva fel utan att spelet avbryts
-
-    #göra så att när spelet går in i andra omgången så ska man inte få frågan om man vill spela ett spel, utan bara om man vill kasta tärningarna
-
-    #fixa så man kan spela fler än 1 person
-
-
         if len(participant.table) == 0:
             print("Du har tyvär inte några alternativ kvar att lägga poäng på, du får därför vänta tills alla spelat klart")
         elif len(participant.table) > 0:
-
-
 
             while throws > 0:
 
@@ -240,11 +207,10 @@ while game_time == True:
                                     dice.remove(save)
                                     amount = amount - 1
 
-                                    
-                                    # Denna for slinga kollar igenom listan "dice" och lägger in dem i 
-                                    # listan "saved", men for slingan kollar igenom listan "dice" backlänges 
-                                    # för att int ändra index-värdet hos objekten i listan och garantera att 
-                                    # alla bjekt av samma värde tas bort oavsätt index
+                                    """Denna for slinga kollar igenom listan "dice" och lägger in dem i 
+                                    listan "saved", men for slingan kollar igenom listan "dice" backlänges 
+                                    för att int ändra index-värdet hos objekten i listan och garantera att 
+                                    alla bjekt av samma värde tas bort oavsätt index"""
                                                             
                             print(dice)
                             if len(saved) > 0:
@@ -309,34 +275,14 @@ while game_time == True:
                         print()
                         throw = "n"
                         
-
-        #En lista med alla olika poängalternativ,
-        #koden ska kolla listan med tärningar och sedan utesluta dalternativ från listan med poäng alternativ.
-        #Detta genom att kolla vilka slags tärningar som listan med resulterande tärningar inehåller.
-
-        #sedan ska programet visa dessa utvalda alternativ för spelaren och låta spelaren välja ett av alternativen
-        #Om programmet bedömer att det inte finns några alternativ som faller samman med tärningarna,
-        #ska programmet föra fram en lista med de återstående alternativen och låta spelaren stryka ett alternativ.
-
-        #När ett alternativ är valt eller struket, ska listan ta bort detta alternativ och spara det i den slutgiltiga listan.
-        #Sedan ska spelaren presenteras med den slutgiltiga listan och summan av poängen.
             print(dice)
             potential_choice = dice[0]
 
             while potential_choice < 7:
                 for die in dice:
                     if die == potential_choice:
-                        #tillägning_av_alternativ Python PEP: https://peps.python.org/pep-0008/
                         count_of_options(potential_choice)
                 potential_choice = potential_choice + 1
-
-
-        #Jag föränklade koden nedan för att minska mängden kod att underhålla, detta gjorde jag genom en while loop som ökar värden för 
-        # varge tal i listan med tärningar. Då den bara kollar från ett till sex, går det att använda mig av index för att kolla omalternativen finns,
-        #detta på grund av att i listan med alternativ kommer 
-
-
-
 
             timer = 1
             count = 2
